8ee6a89603cbc744e56523b6cc193b650f3d17d5
jamaunon <57899161+jamaunon@users.noreply.github.com>
2019-11-19 13:20:56 +0100

    Update HelloController.java
    uno

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/welcome")
 
 public class HelloController {
-
+ 
 @RequestMapping(method = RequestMethod.GET)
 public String printWelcome(final ModelMap model) {
 model.addAttribute("message", "Spring Security Hello World");
 return "hello";
 }
- }
+}
  
