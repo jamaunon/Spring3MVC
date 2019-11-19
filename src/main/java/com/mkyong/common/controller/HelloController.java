014fb7e86ec025b0c65ee1059f2c0b1cd5e494e8
jamaunon <57899161+jamaunon@users.noreply.github.com>
2019-11-19 13:12:40 +0100

    Update HelloController.java
    a ver ahora

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
 @Controller
 @RequestMapping("/welcome")
- 
+
 public class HelloController {
  
 @RequestMapping(method = RequestMethod.GET)
-  public String printWelcome(final ModelMap model) {
- model.addAttribute("message", "Spring Security Hello World");
- return "hello";
+public String printWelcome(final ModelMap model) {
+model.addAttribute("message", "Spring Security Hello World");
+return "hello";
  }
  }
  
